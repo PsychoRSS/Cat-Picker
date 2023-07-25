e756c53ebe9e0b0190785bf3e9977c8d1d2c600e
Javi <nb2javi@gmail.com>
2023-07-24 19:36:11 -0500

    First merge with Alex script

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,46 @@
+const favoritesList = $("#favoritesList");
+const petImage = $("#petImage");
+const generateButton = $("#generateButton");
+const breedText = $("#currentBreedText");
+const favoriteButton = $("#favoriteButton");
+const factText = $("#funFactText");
+const testUrl = "https://cataas.com/cat?json=true";
 
+$(generateButton).on("click", () => {
+  fetch(testUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+});
 
 function getCatPic() {
-    var requestUrl = 'https://meowfacts.herokuapp.com/';
-  
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-        for (let o = 0; o < data.length; o++) {
+  var requestUrl = "https://meowfacts.herokuapp.com/";
 
-        }
-   
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      for (let o = 0; o < data.length; o++) {}
+    });
+}
 
-      });
-  }
- 
-
-  
 function getCatFact() {
-    var requestUrl = 'https://cataas.com/cat?json=true';
-  
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-        for (let o = 0; o < data.length; o++) {
+  var requestUrl = "https://cataas.com/cat?json=true";
 
-        }
-   
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      for (let o = 0; o < data.length; o++) {}
+    });
+}
 
-      });
-  } 
-  
-  getCatPic()
-  getCatFact()
+getCatPic();
+getCatFact();
