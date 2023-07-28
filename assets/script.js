63607ffacfb5cd4c155c4d76be0debe231ce13b2
Javi <nb2javi@gmail.com>
2023-07-27 19:04:30 -0500

    Added comments to script and now calls picture and fact functions on page load

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,7 @@ const welcomePage = $("#welcomePage");
 const catPage = $("#catPage");
 let currentBreed;
 
+// Renders favorites list by taking values from localstorage
 function loadFavoritesList() {
   let favoriteBreeds = JSON.parse(localStorage.getItem("favoriteBreeds"));
   $(favoritesList).html("");
@@ -28,6 +29,7 @@ function loadFavoritesList() {
   }
 }
 
+// Renders picture of cat and defines currentBreed variable for later use
 function getCatPic() {
   var requestUrl =
     "https://api.thecatapi.com/v1/images/search?limit=10&has_breeds=1&api_key=live_lkUcIGhHny1aB9p7gGVkrT3tBLGtuYNCBS6j3kFRxxWdKxXWwWRoCJFwUB4YUIOO";
@@ -42,6 +44,8 @@ function getCatPic() {
       $(breedText).text(`Breed: ${data[0].breeds[0].name}`);
     });
 }
+
+// Renders cat fact in fact box
 function getCatFact() {
   var requestUrl = "https://meowfacts.herokuapp.com/";
 
@@ -54,11 +58,13 @@ function getCatFact() {
     });
 }
 
+// Adds event listener to "generate cat" button to load new picture and fact
 $(generateButton).on("click", () => {
   getCatPic();
   getCatFact();
 });
 
+// Adds event listener to "favorite" button to store the breed of the currently shown cat in localstorage, then re-renders list
 $(favoriteButton).on("click", () => {
   let favoriteBreeds = JSON.parse(localStorage.getItem("favoriteBreeds"));
   if (!currentBreed) {
@@ -74,10 +80,13 @@ $(favoriteButton).on("click", () => {
   }
 });
 
+// Hides main app page and loads welcome page on site load
 function welcome() {
   welcomePage.show();
   catPage.hide();
 }
+
+// Adds event listener to "start" button on welcome page to proceed to main application
 $(welcomeButton).on("click", function () {
   catPage.show();
   welcomePage.hide();
@@ -85,3 +94,5 @@ $(welcomeButton).on("click", function () {
 
 welcome();
 loadFavoritesList();
+getCatPic();
+getCatFact();
